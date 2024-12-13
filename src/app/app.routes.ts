82439766b00acf4f6308dc0c818cfe0af2ff991b
MAYERLY MELISSA GUAMAN CAMACHO <111653397+MayyGuaman@users.noreply.github.com>
2024-12-12 20:42:47 -0500

    winners-table
    winners-table component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { GameBoardComponent } from './game-board/game-board.component';
+import { WinnersTableComponent } from './winners-table/winners-table.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'game-board', component: GameBoardComponent },
+  { path: 'winners-table', component: WinnersTableComponent},
 ];
